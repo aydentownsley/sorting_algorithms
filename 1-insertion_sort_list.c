47995b30cfdead5d3893205fbe25c3b1b9297da5
ranicholson <ranicho2@gmail.com>
2021-01-29 18:08:21 -0600

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,24 +7,53 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *next = *list, *curr = *list, *dll = *list;
+	listint_t *nnod = NULL, *curr = NULL, *dll = *list, *pnod = NULL;
 
-	while (dll->next != NULL)
+	while (dll != NULL)
 	{
-		next = dll->next;
-		curr = dll;
-		if (curr->n > next->n)
+		if (dll->n > dll->next->n)
 		{
-			curr->next = next->next;
-			curr->prev->next = next;
-			next->next = curr;
-			next->prev = curr->prev;
-			curr->prev = next;
+			curr = dll;
+			nnod = dll->next;
+			if (nnod->next == NULL)
+				curr->next = NULL;
+			else
+				curr->next = nnod->next;
+			if (curr->prev == NULL)
+			{
+				nnod->prev = NULL;
+				*list = nnod;
+			}
+			else
+				curr->prev->next = nnod;
+			nnod->next = curr;
+			nnod->prev = curr->prev;
+			curr->prev = nnod;
 			curr->next->prev = curr;
 			print_list(*list);
-			if (dll->prev->n > next->n)
-				insertion_sort_list(list);
+			while (nnod->n < nnod->prev->n)
+			{
+				pnod = nnod->prev;
+				curr = nnod;
+				if (pnod->prev == NULL)
+				{
+					curr->prev = NULL;
+					*list = curr;
+				}
+				else
+				{
+					curr->prev = pnod->prev;
+					pnod->prev->next = nnod;
+				}
+				pnod->next = curr->next;
+				curr->next = pnod;
+				curr->next->prev = pnod;
+				pnod->prev = curr;
+				print_list(*list);
+				nnod = nnod->prev;
+			}
 		}
 		dll = dll->next;
+
 	}
 }
