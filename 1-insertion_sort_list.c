5ba7ecdbe80d8bfb1ef18b914894ae8387e03d37
ranicholson <ranicho2@gmail.com>
2021-01-30 13:55:13 -0600

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,29 +1,35 @@
 #include "sort.h"
 
 /**
- *
- *
+ * insertion_sort_list - function to insertion sort a list
+ * @list: list to sort
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *next = *list, *curr = *list, *dll = *list;
+	listint_t *curr, *dll = (*list)->next, *pnod;
 
-	while (dll->next != NULL)
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	while (dll)
 	{
-		next = dll->next;
 		curr = dll;
-		if (curr->n > next->n)
+		pnod = dll->prev;
+		while (pnod && curr->n < pnod->n)
 		{
-			curr->next = next->next;
-			curr->prev->next = next;
-			next->next = curr;
-			next->prev = curr->prev;
-			curr->prev = next;
-			curr->next->prev = curr;
+			if (pnod->prev != NULL)
+				pnod->prev->next = curr;
+			if (curr->next != NULL)
+				curr->next->prev = pnod;
+			if (pnod->prev == NULL)
+				(*list) = curr;
+			pnod->next = curr->next;
+			curr->next = pnod;
+			curr->prev = pnod->prev;
+			pnod->prev = curr;
 			print_list(*list);
-			if (dll->prev->n > next->n)
-				insertion_sort_list(list);
+			pnod = curr->prev;
 		}
 		dll = dll->next;
 	}
