a8f323332c5d742d35ec6cc581dbdb16ecfdb8d4
aydentownsley <aydentownsley@gmail.com>
2021-02-01 16:55:00 +0000

    fixed issue when duplicate numbers exist

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 		flag = 0;
 		for (idx = 0; idx < size - 1; idx++)
 		{
-			if (array[idx] < array[idx + 1])
+			if (array[idx] <= array[idx + 1])
 				continue;
 			else
 			{
