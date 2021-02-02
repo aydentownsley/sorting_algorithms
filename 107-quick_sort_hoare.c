4e92ba85d2a476c1d0cd4f464e49e29d05772290
ranicholson <ranicho2@gmail.com>
2021-02-02 14:43:00 -0600

    Task 11

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -7,7 +7,7 @@ void qsrec(size_t start, size_t stop, int *array, size_t size);
  * @size: size of array
  */
 
-void quick_sort(int *array, size_t size)
+void quick_sort_hoare(int *array, size_t size)
 {
 	int start = 0;
 	size_t stop = (size - 1);
